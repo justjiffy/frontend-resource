df15b1913bb25d0b4118ca5a6fbc7d73bffa434e
Juli Emmel <justjiffy@gmail.com>
2015-09-24 16:58:58 -0700

    changed user collect => posts

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+var router = express.Router();
+
+var dbConfig = require('./db/config.js');
+// var mongoose = require('mongoose');
+
+var userController = require('./controllers/users.js');
+
+var Promise = require('bluebird');
+var mongoose = Promise.promisifyAll(require('mongoose'));
+
+//TELLING APP.JS THAT WE'RE USING THE ROUTES DIR
+app.use(require('./node_modules/body-parser').urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use('/', userController);
+
+switch(app.get('env')) {
+	case 'development':
+		mongoose.connect(dbConfig.mongo.dev.conn, dbConfig.mongo.options);
+		break;
+	case 'production':
+		mongoose.connect(dbConfig.mongo.prod.conn, dbConfig.mongo.options);
+		break;
+	default:
+		throw newError('Unknown execution environment: ' + app.get('env'));
+}
+
+app.use('/', router);
+app.listen(port);
+console.log('Server started on ' + port);
---
+++ controllers/users.js
@@ -0,0 +1,3 @@
+var express = require('express');
+var usersController = express.Router();
+var User = require('../models/user.js');
